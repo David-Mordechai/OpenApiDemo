9391d7dbb262b58ef96ae6445d11cc68427b33be
David Mordechai <david.mordechai79@gmail.com>
2022-07-08 12:49:40 +0300

    Add project files.

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleClient;
+
+Console.WriteLine("Fetching weather data from Weather Api using OpenApi.\n");
+using (var httpClient = new HttpClient())
+{
+    var swaggerClient = new swaggerClient("http://localhost:5128/", httpClient);
+    var weatherForecasts = await swaggerClient.GetWeatherForecastAsync();
+
+    foreach (var weatherForecast in weatherForecasts)
+    {
+        Console.WriteLine($"Date: {weatherForecast.Date}, " +
+                          $"Summary: {weatherForecast.Summary}, " +
+                          $"TemperatureF: {weatherForecast.TemperatureF}, " +
+                          $"TemperatureC: {weatherForecast.TemperatureC}.");
+    }
+}
+
+Console.ReadKey();
